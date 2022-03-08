234647b132696ab26cd0d704067a6a56fa6de623
Alex Paper <alessandro.carta@gmail.com>
2022-03-08 14:58:55 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ModalDisclaimer from './Components/ModalDisclaimer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>React Modal Window</h1>
+    <ModalDisclaimer />
+    <h2>Lorem Ipsum</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus corporis quos, modi, quo ratione ipsam quod cumque delectus vel eligendi, ut architecto accusamus quia! Laudantium nihil pariatur repellendus corporis minima eius excepturi dolor quia iure, magni sequi nesciunt saepe sapiente quam neque eligendi commodi beatae consectetur repellat ipsam cupiditate aliquam! Inventore aut tempora corporis laboriosam adipisci excepturi quaerat? Ipsum cumque perspiciatis aperiam, exercitationem itaque laborum corrupti autem aliquid ullam? Odit delectus corporis maiores quis nemo alias optio corrupti porro qui? Corporis doloribus officia ea libero aperiam, veritatis autem error laudantium! Maxime praesentium inventore veritatis distinctio tenetur possimus aperiam qui nesciunt est ratione, dignissimos aliquid in dolore magnam sint vel dolores quo provident mollitia aliquam repellendus modi natus accusantium quas! Ex veniam tempore commodi corrupti blanditiis! Incidunt et deserunt sit expedita?</p>
     </div>
   );
 }
